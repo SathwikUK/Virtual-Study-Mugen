26c3cae02eafc039f6abdcf522a51491f4729d95
SathwikUK <sathwiksampengala@gmail.com>
2025-02-18 00:06:53 +0530

    frontend link

--- backend/models/Message.js
+++ backend/models/Message.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 
 const MessageSchema = new mongoose.Schema({
-  groupId: { type: mongoose.Schema.Types.ObjectId, ref: "vsmGroup", required: true },
-  senderId: { type: mongoose.Schema.Types.ObjectId, ref: "vsmUser", required: true },
+  groupId: { type: mongoose.Schema.Types.ObjectId, ref: "vsGroup", required: true },
+  senderId: { type: mongoose.Schema.Types.ObjectId, ref: "vsUser", required: true },
   message: { type: String, required: false },
   fileData: {
     data: Buffer,
@@ -12,7 +12,7 @@ const MessageSchema = new mongoose.Schema({
   timestamp: { type: Date, default: Date.now },
   edited: { type: Boolean, default: false },
   senderName: { type: String },
-  readBy: [{ type: mongoose.Schema.Types.ObjectId, ref: "vsmUser" }] // Add readBy array
+  readBy: [{ type: mongoose.Schema.Types.ObjectId, ref: "vsUser" }] // Add readBy array
 });
 
-module.exports = mongoose.model("vsmMessage", MessageSchema);
+module.exports = mongoose.model("vsMessage", MessageSchema);
--- backend/models/groupModel.js
+++ backend/models/groupModel.js
@@ -4,12 +4,12 @@ const groupSchema = new mongoose.Schema({
   name: { type: String, required: true },
   image: { type: String, required: true },
   description: { type: String, required: true }, // Added description field
-  createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'studentUser', required: true }, // Reference to User model
-  members: [{ type: mongoose.Schema.Types.ObjectId, ref: 'studentUser' }], // Array of User references
+  createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'vsUser', required: true }, // Reference to User model
+  members: [{ type: mongoose.Schema.Types.ObjectId, ref: 'vsUser' }], // Array of User references
   createdAt: { type: Date, default: Date.now },
   isLive: { type: Boolean, default: false },
 });
 
-const Group = mongoose.model("vsmGroups", groupSchema);
+const Group = mongoose.model("vsGroups", groupSchema);
 
 module.exports = Group;
