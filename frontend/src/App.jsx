5647bb01504af127a4b57f5d0125f2d577ef49c4
SathwikUK <sathwiksampengala@gmail.com>
2025-02-17 18:19:51 +0530

    backendeploy ready

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,61 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Home from "./pages/Home";
+
 import Login from "./pages/Login";
+
 import Signup from "./pages/Signup";
+
 import AdminDashboard from "./pages/AdminDashboard";
+
 import StudentDashboard from "./pages/StudentDashboard";
+
 import ForgotPassword from "./pages/ForgotPassword";
+
 import { ToastContainer } from "react-toastify";
+
 import "react-toastify/dist/ReactToastify.css";
+
 import VsmChat from "./pages/VsmChat";
 
+import ResetPassword from "./pages/ResetPassword";
+
+
+
 function App() {
+
   return (
+
     <Router>
+
       <ToastContainer />
+
       <Routes>
+
         <Route path="/admin-dashboard" element={<AdminDashboard />} />
+
         <Route path="/dashboard" element={<StudentDashboard />} />
+
         <Route path="/" element={<Home />} />
+
         <Route path="/login" element={<Login />} />
+
         <Route path="/signup" element={<Signup />} />
+
         <Route path="/openai" element={<VsmChat />} />
+
         <Route path="/forgot-password" element={<ForgotPassword />} />
+
+        <Route path="/reset-password" element={<ResetPassword />} />
+
       </Routes>
+
     </Router>
+
   );
+
 }
+
+
 
 export default App;
